22dce4916e1a9c86b74fa091d6972c87f152c4fb
Anggoran <anggoranm@gmail.com>
2023-06-11 23:29:43 +0700

    🐍 python test with csv

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -6,7 +7,19 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def hello():
     response = {"message": "Hello, World!"}
-    return jsonify(response)
+    return jsonify(response), 200
+
+
+@app.route("/upload-csv", methods=["POST"])
+def upload_csv():
+    file = request.files["file"]
+
+    if file and file.filename.endswith(".csv"):
+        file.save(file.filename)
+        df = pd.read_csv(file.filename)
+        return df.to_json(orient="records"), 201
+
+    return jsonify({"status": "error", "message": "Please upload a CSV file."}), 400
 
 
 @app.after_request
