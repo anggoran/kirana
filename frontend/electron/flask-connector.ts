0c3cdc6bc4941b69e4175792c0c3313560c5a595
Anggoran <anggoranm@gmail.com>
2023-08-13 11:04:45 +0700

    remove electorn config

--- frontend/electron/flask-connector.ts
+++
@@ -1,9 +0,0 @@
-import { join } from "path";
-import { execFile } from "child_process";
-
-export const flaskConnect = (root: string) => {
-  const flask = join(root, "../../Resources/Kirana Server");
-  return execFile(flask, {
-    windowsHide: true,
-  });
-};
